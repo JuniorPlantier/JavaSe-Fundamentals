2ed00dbbccfcb3d3718e30222e1904b1adafab51
Jr Plantier <junior.plantier@gmail.com>
2020-08-07 13:00:09 -0300

    Add Pessoa e Figurinha

---
+++ 04.01-classesEobjetos/src/Aplicacao.java
@@ -0,0 +1,19 @@
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+		
+		Pessoa p1 = new Pessoa();
+		p1.nome = "Baron Davis";
+		p1.receber(5);
+		p1.receber(7);
+		
+		Pessoa p2 = new Pessoa();
+		p2.nome = "Maria";
+		
+		p1.dar(3, p2);
+		
+		System.out.println(p1.nome +" => "+p1.numFigurinhas);
+		System.out.println(p2.nome +" => "+p2.numFigurinhas);
+	}
+}
---
+++ 04.01-classesEobjetos/src/Pessoa.java
@@ -0,0 +1,15 @@
+
+public class Pessoa {
+
+	String nome;
+	int numFigurinhas;
+	
+	void receber(int numFigurinhas) {
+		this.numFigurinhas += numFigurinhas;
+	}
+	
+	void dar(int numFigurinhas, Pessoa p) {
+		this.numFigurinhas -= numFigurinhas;
+		p.receber(numFigurinhas);
+	}
+}
