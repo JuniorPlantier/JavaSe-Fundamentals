ebbf280a0895a742cffa898b6fcffff95f938e5b
Jr Plantier <junior.plantier@gmail.com>
2020-08-07 12:15:10 -0300

    Commit inicial

---
+++ 03.03-SwitchExpressions/src/Aplicacao.java
@@ -0,0 +1,43 @@
+
+public class Aplicacao {
+	
+	/* Aparecerem pela primeira vez na versão 12 e na 13 foram aprimoradas.
+	 * 
+	 * A forma antiga continua existindo.
+	 * */
+	
+	public enum DiaDaSemana {
+		SEGUNDA,
+		TERCA,
+		QUARTA,
+		QUINTA,
+		SEXTA,
+		SABADO,
+		DOMINGO
+	}
+	
+	public static void main(String[] args) {
+		DiaDaSemana d = DiaDaSemana.QUARTA;
+		
+//		switch(d) {
+//			case SEGUNDA: System.out.println(1); break;
+//			case TERCA: System.out.println(2); break;
+//		}
+		
+		switch(d) {
+			case SEGUNDA -> { 
+				System.out.println(1);
+			}
+			case TERCA -> System.out.println(2);
+			default -> System.out.println("outro");
+		}
+		
+		int i = switch(d) {
+			case SEGUNDA -> 1;
+			case TERCA -> 2;
+			default -> 0;
+		};
+		
+		System.out.println(i);
+	}
+}
