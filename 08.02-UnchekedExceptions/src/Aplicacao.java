26df432634903e774f44ab3b55ab05b08a90c28d
Jr Plantier <junior.plantier@gmail.com>
2020-08-10 18:49:31 -0300

    Runtime Exception

---
+++ 08.02-UnchekedExceptions/src/Aplicacao.java
@@ -0,0 +1,12 @@
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+		Object o1 = new Object();
+		Object o2 = new Object();
+		
+		boolean iguais = Comparador.comparar(null, o2);
+		System.out.println(iguais);
+		
+	}
+}
---
+++ 08.02-UnchekedExceptions/src/Comparador.java
@@ -0,0 +1,12 @@
+
+public class Comparador {
+
+	public static boolean comparar(Object o1, Object o2) {
+		if(o1 == null) {
+			return false;
+		}
+		
+		return o1.equals(o2);
+	}
+	
+}
