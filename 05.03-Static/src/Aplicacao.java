13bf99fab44e1511a296fa6f4063c301d5e9edc8
Jr Plantier <junior.plantier@gmail.com>
2020-08-10 11:45:35 -0300

    Elementos estáticos

---
+++ 05.03-Static/src/Aplicacao.java
@@ -0,0 +1,27 @@
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+		
+//		Matematica m = new Matematica();
+//		int soma = m.somar(5, 7);
+//		int subtracao = m.subtrair(7, 2);
+		
+//		int soma = Matematica.somar(5, 7);
+//		int subtracao = Matematica.subtrair(7, 2);
+//		
+//		System.out.println(soma);
+//		System.out.println(subtracao);
+		
+		
+//		Contador c1 = new Contador();
+//		Contador c2 = new Contador();
+//		Contador c3 = new Contador();
+//		Contador.incrementar();
+//		
+//		System.out.println(Contador.getValor());
+		
+		double media = Constantes.MEDIA_DA_PROVA;
+		
+	}
+}
---
+++ 05.03-Static/src/Matematica.java
@@ -0,0 +1,11 @@
+
+public class Matematica {
+
+	public static int somar(int n1, int n2) {
+		return n1 + n2;
+	}
+	
+	public static int subtrair(int n1, int n2) {
+		return n1 - n2;
+	}
+}
