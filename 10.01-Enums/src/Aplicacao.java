0ab9cac7daa82c300d77fe73deaec4690e13bb76
Jr Plantier <junior.plantier@gmail.com>
2020-08-16 11:42:16 -0300

    Enums

---
+++ 10.01-Enums/src/Aplicacao.java
@@ -0,0 +1,19 @@
+
+public class Aplicacao {
+
+	private enum Sexo {
+		MASCULINO, FEMININO
+	}
+	
+	public static void main(String[] args) {
+		
+		DiaDaSemana hoje = DiaDaSemana.DOMINGO;
+		
+		int num = DiaDaSemana.QUARTA.getValor();
+		System.out.println(num);
+		
+		String s = "TERCA"; // "terca"; -> lança uma exceção.
+		DiaDaSemana d = DiaDaSemana.valueOf(s);
+		System.out.println(d.getValor());
+	}
+}
---
+++ 10.01-Enums/src/DiaDaSemana.java
@@ -0,0 +1,21 @@
+
+public enum DiaDaSemana {
+
+	DOMINGO(1),
+	SEGUNDA(2),
+	TERCA(3),
+	QUARTA(4),
+	QUINTA(5), 
+	SEXTA(6),
+	SABADO(7);
+	
+	private int valor;
+	
+	private DiaDaSemana(int valor) {
+		this.valor = valor;
+	}
+	
+	public int getValor() {
+		return valor;
+	}
+}
